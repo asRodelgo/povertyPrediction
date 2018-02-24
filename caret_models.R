c5b677773d9e960cb7faca16baa8139b0dc42877
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2018-02-24 15:29:18 -0500

    modified data pre-processing and modeled gbm using caret

---
+++ caret_models.R
@@ -0,0 +1,69 @@
+# caret models -----------
+#
+library(caret)
+#### Random Forest model ####
+model <- "gbm"
+require(pROC)
+require(e1071)
+
+grid <- expand.grid(.interaction.depth = c(7),
+                    .n.trees=seq(500),
+                    .shrinkage=c(0.005),
+                    .n.minobsinnode=5)
+
+ctrl <- trainControl(method="repeatedcv",
+                     number=10,
+                     repeats=10,
+                     classProbs=TRUE,
+                     summaryFunction=twoClassSummary)
+
+# without parallelization this takes about an hour to run
+set.seed(123)
+poor_train <- mutate(poor_train, poor = ifelse(poor == 1,"poor","no_poor"))
+gbmTune <- caret::train(poor ~., data=poor_train,
+                 method="gbm",
+                 metric="ROC",
+                 tuneGrid=grid,
+                 verbose=FALSE,
+                 trControl=ctrl)
+#######
+# The results on train
+ggplot(gbmTune) + theme(legend.position="top")
+gbmPred <- predict(gbmTune, poor_train)
+gbmProbs <- predict(gbmTune, poor_train, type="prob")
+head(gbmProbs)
+cm <- confusionMatrix(gbmPred, poor_train$poor)
+cm
+rocCurve <- roc(response=poor_train$poor,
+                predictor=gbmProbs[,"poor"],
+                levels=rev(levels(factor(poor_train$poor))))
+rocCurve
+plot(rocCurve,
+     print.thres=c(.5,.2),
+     print.thres.pch=16,
+     print.thres.cex=1.2)
+
+# predicted classes on test
+gbmPred <- predict(gbmTune, poor_test)
+# predicted probabilities on test
+gbmProbs <- predict(gbmTune, poor_test, type="prob")
+head(gbmProbs)
+# put all predictions together
+preds_df <- tibble(pred_prob = as.numeric(gbmProbs$poor), pred_class = as.character(gbmPred),
+                   test_class = poor_test$poor)
+# compute the mean log loss on the test data
+.logLoss(preds_df[,c(1,3)])
+
+# prepare submission file: id,country,poor
+# add poor column to test_all just to fit the features() function defined above
+test_all_poor <- mutate(test_all, poor = "unknown")
+final_predictions <- predict(gbmTune, test_all_poor, type="prob")
+submission_file <- tibble(id = test_all_poor$id,
+                          country = cou,
+                          poor = as.numeric(final_predictions$poor))
+
+
+
+
+
+
--- data_processing.R
+++ data_processing.R
@@ -6,94 +6,239 @@ library(caret)
 library(doMC)
 registerDoMC(cores = 2)
 
-### download individual data -----------------------
-# training
-#download.file("https://s3.amazonaws.com/drivendata/data/50/public/A_indiv_train.csv", "A_indiv_train.csv")
-a_indiv_train <- fread("A_indiv_train.csv", stringsAsFactors = TRUE)
-# testing
-#download.file("https://s3.amazonaws.com/drivendata/data/50/public/A_indiv_test.csv", "A_indiv_test.csv")
-a_indiv_test <- fread("A_indiv_test.csv", stringsAsFactors = TRUE)
+# ### download train data -------------
+# split <- "train" 
+# type <- "indiv"
+# type_train <- data.frame()
+# for (cou in c("A","B","C")){
+#     download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_",type,"_",split,".csv"), paste0(cou,"_",type,"_",split,".csv"))
+#     this_train <- fread(paste0(cou,"_",type,"_",split,".csv"), stringsAsFactors = TRUE) %>%
+#       mutate(country = cou)
+#     if (nrow(type_train)>0) type_train <- bind_rows(type_train,this_train) else type_train <- this_train
+# }
+# indiv_train <- type_train
+# 
+# type <- "hhold"
+# type_train <- data.frame()
+# for (cou in c("A","B","C")){
+#   download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_",type,"_",split,".csv"), paste0(cou,"_",type,"_",split,".csv"))
+#   this_train <- fread(paste0(cou,"_",type,"_",split,".csv"), stringsAsFactors = TRUE) %>%
+#     mutate(country = cou)
+#   if (nrow(type_train)>0) type_train <- bind_rows(type_train,this_train) else type_train <- this_train
+# }
+# hhold_train <- type_train
+# 
+# ### download test data -------------
+# split <- "test" 
+# type <- "indiv"
+# type_test <- data.frame()
+# for (cou in c("A","B","C")){
+#   download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_",type,"_",split,".csv"), paste0(cou,"_",type,"_",split,".csv"))
+#   this_test <- fread(paste0(cou,"_",type,"_",split,".csv"), stringsAsFactors = TRUE) %>%
+#     mutate(country = cou)
+#   if (nrow(type_test)>0) type_test <- bind_rows(type_test,this_test) else type_test <- this_test
+# }
+# indiv_test <- type_test
+# 
+# type <- "hhold"
+# type_test <- data.frame()
+# for (cou in c("A","B","C")){
+#   download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_",type,"_",split,".csv"), paste0(cou,"_",type,"_",split,".csv"))
+#   this_test <- fread(paste0(cou,"_",type,"_",split,".csv"), stringsAsFactors = TRUE) %>%
+#     mutate(country = cou)
+#   if (nrow(type_test)>0) type_test <- bind_rows(type_test,this_test) else type_test <- this_test
+# }
+# hhold_test <- type_test
+# 
+# ### Pre-processing individual data ------------------------
+# # append train and test
+# indiv_all <- bind_rows(indiv_train,indiv_test) %>%
+#   mutate_if(is.character, as.factor) %>%
+#   mutate_if(is.factor, addNA)
+# # keep train ids and classes for later
+# indiv_train_ids_class <- select(indiv_train, id, poor) %>%
+#   distinct(id, .keep_all = TRUE) %>%
+#   mutate(id = as.character(id), poor = ifelse(poor,1,0))
+# # dummify variables
+# indiv_all_ids <- indiv_all[,c(1)]
+# indiv_all_class <- ifelse(indiv_all$poor == TRUE,1,0)
+# dummies <- dummyVars(id ~ ., data = select(indiv_all, -poor,-country))
+# dummies_pred <- predict(dummies, newdata = indiv_all) %>% as.data.frame()
+# indiv_all_dummy <- cbind(id = indiv_all_ids,dummies_pred) %>%
+#   mutate(id = as.character(id))
+# # zero & near-zero variance
+# nzv <- nearZeroVar(indiv_all_dummy)
+# indiv_all_nzv <- indiv_all_dummy[, -nzv]
+# indiv_all_nzv <- select(indiv_all_nzv, -iid)
+# # impute NAs in numeric variables
+# indiv_all_nzv2 <- mutate_all(indiv_all_nzv, funs(ifelse(is.na(.),mean(.,na.rm=TRUE),.)))
+# # highly correlated predictors
+# descrCor <- cor(select_if(indiv_all_nzv2, is.numeric))
+# summary(descrCor[upper.tri(descrCor)])
+# highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
+# indiv_all_corr <- select_if(indiv_all_nzv2, is.numeric)[,-highlyCorDescr] %>%
+#   cbind(select_if(indiv_all_nzv2, is.character))
+# # linear dependencies
+# comboInfo <- findLinearCombos(select_if(indiv_all_corr, is.numeric))
+# if (!is.null(comboInfo$remove)) {
+#   indiv_all_ldep <- indiv_all_corr[, -comboInfo$remove]
+# } else {
+#   indiv_all_ldep <- indiv_all_corr
+# }
+# # aggregate at household level
+# indiv_all_aggr <- indiv_all_ldep %>%
+#   mutate(id = as.character(id)) %>%
+#   group_by(id) %>%
+#   summarise_all(mean)
+# # split it back into train and test
+# train_indiv <- merge(indiv_train_ids_class, indiv_all_aggr, by = "id")
+# test_indiv <- merge(distinct(indiv_test, id) %>% mutate(id = as.character(id)) , indiv_all_aggr, by = "id")
+# 
+# ### Pre-processing hhold data ------------------------
+# # append train and test
+# hhold_all <- bind_rows(hhold_train,hhold_test) %>%
+#   mutate_if(is.character, as.factor) %>%
+#   mutate_if(is.factor, addNA)
+# # keep train ids and classes for later
+# hhold_train_ids_class <- select(hhold_train, id, poor) %>%
+#   distinct(id, .keep_all = TRUE) %>%
+#   mutate(id = as.character(id), poor = ifelse(poor,1,0))
+# # dummify variables
+# hhold_all_ids <- hhold_all[,c(1)]
+# hhold_all_class <- ifelse(hhold_all$poor == TRUE,1,0)
+# dummies <- dummyVars(id ~ ., data = select(hhold_all, -poor,-country))
+# dummies_pred <- predict(dummies, newdata = hhold_all) %>% as.data.frame()
+# hhold_all_dummy <- cbind(id = hhold_all_ids,dummies_pred) %>%
+#   mutate(id = as.character(id))
+# # zero & near-zero variance
+# nzv <- nearZeroVar(hhold_all_dummy)
+# hhold_all_nzv <- hhold_all_dummy[, -nzv]
+# # impute NAs in numeric variables
+# hhold_all_nzv2 <- mutate_all(hhold_all_nzv, funs(ifelse(is.na(.),mean(.,na.rm=TRUE),.)))
+# # highly correlated predictors
+# descrCor <- cor(select_if(hhold_all_nzv2, is.numeric))
+# summary(descrCor[upper.tri(descrCor)])
+# highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
+# hhold_all_corr <- select_if(hhold_all_nzv2, is.numeric)[,-highlyCorDescr] %>%
+#   cbind(select_if(hhold_all_nzv2, is.character))
+# # linear dependencies
+# comboInfo <- findLinearCombos(select_if(hhold_all_corr, is.numeric))
+# if (!is.null(comboInfo$remove)) {
+#   hhold_all_ldep <- hhold_all_corr[, -comboInfo$remove]
+# } else {
+#   hhold_all_ldep <- hhold_all_corr
+# }
+# # split it back into train and test
+# train_hhold <- merge(hhold_train_ids_class, hhold_all_ldep, by = "id")
+# test_hhold <- merge(distinct(hhold_test, id) %>% mutate(id = as.character(id)) , hhold_all_ldep, by = "id")
+# 
+# # put indiv and hhold together
+# train_all <- merge(train_hhold,train_indiv, by = c("id","poor"))
+# test_all <- merge(test_hhold,test_indiv, by = c("id"))
+# #write.csv(train_all, "train_all.csv", row.names = FALSE)
+# #write.csv(test_all, "test_all.csv", row.names = FALSE)
 
+################################################################################################
+################################################################################################
+## Same pre-processing country by country
+
+### download individual data -----------------------
+cou <- "A"
+# training
+download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_indiv_train.csv"), "indiv_train.csv")
+indiv_train <- fread("indiv_train.csv", stringsAsFactors = TRUE)
+# testing
+download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_indiv_test.csv"), "indiv_test.csv")
+indiv_test <- fread("indiv_test.csv", stringsAsFactors = TRUE)
+# 
 ### download household data -----------------------
 # training
-#download.file("https://s3.amazonaws.com/drivendata/data/50/public/A_hhold_train.csv", "A_hhold_train.csv")
-a_hhold_train <- fread("A_hhold_train.csv", stringsAsFactors = TRUE)
+download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_hhold_train.csv"), "hhold_train.csv")
+hhold_train <- fread("hhold_train.csv", stringsAsFactors = TRUE)
 # testing
-#download.file("https://s3.amazonaws.com/drivendata/data/50/public/A_hhold_test.csv", "A_hhold_test.csv")
-a_hhold_test <- fread("A_hhold_test.csv", stringsAsFactors = TRUE)
+download.file(paste0("https://s3.amazonaws.com/drivendata/data/50/public/",cou,"_hhold_test.csv"), "hhold_test.csv")
+hhold_test <- fread("hhold_test.csv", stringsAsFactors = TRUE)
 
 ### Pre-processing individual data ------------------------
 # append train and test
-a_indiv_all <- bind_rows(a_indiv_train,a_indiv_test)
+indiv_all <- bind_rows(indiv_train,indiv_test)
 # keep train ids and classes for later
-a_indiv_train_ids_class <- select(a_indiv_train, id, poor) %>%
+indiv_train_ids_class <- select(indiv_train, id, poor) %>%
   distinct(id, .keep_all = TRUE) %>%
   mutate(id = as.character(id), poor = ifelse(poor,1,0))
 # dummify variables
-a_indiv_all_ids <- a_indiv_all[,c(1)]
-a_indiv_all_class <- ifelse(a_indiv_all$poor == TRUE,1,0)
-dummies <- dummyVars(id ~ ., data = select(a_indiv_all, -poor,-country))
-dummies_pred <- predict(dummies, newdata = a_indiv_all) %>% as.data.frame()
-a_indiv_all_dummy <- cbind(a_indiv_all_ids,dummies_pred) %>%
+indiv_all_ids <- indiv_all[,c(1)]
+indiv_all_class <- ifelse(indiv_all$poor == TRUE,1,0)
+dummies <- dummyVars(id ~ ., data = select(indiv_all, -poor,-country))
+dummies_pred <- predict(dummies, newdata = indiv_all) %>% as.data.frame()
+indiv_all_dummy <- cbind(indiv_all_ids,dummies_pred) %>%
   mutate(id = as.character(id))
 # zero & near-zero variance
-nzv <- nearZeroVar(a_indiv_all_dummy)
-a_indiv_all_nzv <- a_indiv_all_dummy[, -nzv]
-a_indiv_all_nzv <- select(a_indiv_all_nzv, -iid)
+nzv <- nearZeroVar(indiv_all_dummy)
+indiv_all_nzv <- indiv_all_dummy[, -nzv]
+indiv_all_nzv <- select(indiv_all_nzv, -iid)
 # highly correlated predictors
-descrCor <- cor(select_if(a_indiv_all_nzv, is.numeric))
+descrCor <- cor(select_if(indiv_all_nzv, is.numeric))
 summary(descrCor[upper.tri(descrCor)])
 highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
-a_indiv_all_corr <- a_indiv_all_nzv[,-highlyCorDescr]
+#indiv_all_corr <- indiv_all_nzv[,-highlyCorDescr]
+indiv_all_corr <- select_if(indiv_all_nzv, is.numeric)[,-highlyCorDescr] %>%
+   cbind(select_if(indiv_all_nzv, is.character))
+
 # linear dependencies
-comboInfo <- findLinearCombos(select_if(a_indiv_all_corr, is.numeric))
+comboInfo <- findLinearCombos(select_if(indiv_all_corr, is.numeric))
 if (!is.null(comboInfo$remove)) {
-  a_indiv_all_ldep <- a_indiv_all_corr[, -comboInfo$remove]
+  indiv_all_ldep <- indiv_all_corr[, -comboInfo$remove]
 } else {
-  a_indiv_all_ldep <- a_indiv_all_corr
+  indiv_all_ldep <- indiv_all_corr
 }
 # aggregate at household level
-a_indiv_all_aggr <- a_indiv_all_ldep %>%
+indiv_all_aggr <- indiv_all_ldep %>%
   mutate(id = as.character(id)) %>%
   group_by(id) %>%
   summarise_all(mean)
 # split it back into train and test
-train_indiv <- merge(a_indiv_train_ids_class, a_indiv_all_aggr, by = "id")
-test_indiv <- merge(distinct(a_indiv_test, id) %>% mutate(id = as.character(id)) , a_indiv_all_aggr, by = "id")
+train_indiv <- merge(indiv_train_ids_class, indiv_all_aggr, by = "id")
+test_indiv <- merge(distinct(indiv_test, id) %>% mutate(id = as.character(id)) , indiv_all_aggr, by = "id")
 
 
 ### Pre-processing hhold data ------------------------
 # append train and test
-a_hhold_all <- bind_rows(a_hhold_train,a_hhold_test)
+hhold_all <- bind_rows(hhold_train,hhold_test) %>%
+   mutate_if(is.character, as.factor) %>%
+   mutate_if(is.factor, addNA)
+
 # keep train ids and classes for later
-a_hhold_train_ids_class <- select(a_hhold_train, id, poor) %>%
+hhold_train_ids_class <- select(hhold_train, id, poor) %>%
   distinct(id, .keep_all = TRUE) %>%
   mutate(id = as.character(id), poor = ifelse(poor,1,0))
 # dummify variables
-a_hhold_all_ids <- a_hhold_all[,c(1)]
-a_hhold_all_class <- ifelse(a_hhold_all$poor == TRUE,1,0)
-dummies <- dummyVars(id ~ ., data = select(a_hhold_all, -poor,-country))
-dummies_pred <- predict(dummies, newdata = a_hhold_all) %>% as.data.frame()
-a_hhold_all_dummy <- cbind(a_hhold_all_ids,dummies_pred) %>%
+hhold_all_ids <- hhold_all[,c(1)]
+hhold_all_class <- ifelse(hhold_all$poor == TRUE,1,0)
+dummies <- dummyVars(id ~ ., data = select(hhold_all, -poor,-country))
+dummies_pred <- predict(dummies, newdata = hhold_all) %>% as.data.frame()
+hhold_all_dummy <- cbind(id = hhold_all_ids,dummies_pred) %>%
   mutate(id = as.character(id))
 # zero & near-zero variance
-nzv <- nearZeroVar(a_hhold_all_dummy)
-a_hhold_all_nzv <- a_hhold_all_dummy[, -nzv]
+nzv <- nearZeroVar(hhold_all_dummy)
+hhold_all_nzv <- hhold_all_dummy[, -nzv]
 # highly correlated predictors
-descrCor <- cor(select_if(a_hhold_all_nzv, is.numeric))
+descrCor <- cor(select_if(hhold_all_nzv, is.numeric))
 summary(descrCor[upper.tri(descrCor)])
 highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
-a_hhold_all_corr <- a_hhold_all_nzv[,-highlyCorDescr[-which(highlyCorDescr == 1)]]
+hhold_all_corr <- select_if(hhold_all_nzv, is.numeric)[,-highlyCorDescr] %>%
+  cbind(select_if(hhold_all_nzv, is.character))
+
 # linear dependencies
-comboInfo <- findLinearCombos(select_if(a_hhold_all_corr, is.numeric))
+comboInfo <- findLinearCombos(select_if(hhold_all_corr, is.numeric))
 if (!is.null(comboInfo$remove)) {
-  a_hhold_all_ldep <- a_hhold_all_corr[, -comboInfo$remove]
+  hhold_all_ldep <- hhold_all_corr[, -comboInfo$remove]
 } else {
-  a_hhold_all_ldep <- a_hhold_all_corr
+  hhold_all_ldep <- hhold_all_corr
 }
 # split it back into train and test
-train_hhold <- merge(a_hhold_train_ids_class, a_hhold_all_ldep, by = "id")
-test_hhold <- merge(distinct(a_hhold_test, id) %>% mutate(id = as.character(id)) , a_hhold_all_ldep, by = "id")
+train_hhold <- merge(hhold_train_ids_class, hhold_all_ldep, by = "id")
+test_hhold <- merge(distinct(hhold_test, id) %>% mutate(id = as.character(id)) , hhold_all_ldep, by = "id")
 
 # put indiv and hhold together
 train_all <- merge(train_hhold,train_indiv, by = c("id","poor"))
